8f1f46b69700113e7128f724185f1d86e1db74df
Job <pulachecarrenojob837@gmail.com>
2024-11-15 13:50:04 -0500

    AddExamplesPipes

--- src/app/ejemplo-pipes/ejemplo-pipes.component.ts
+++ src/app/ejemplo-pipes/ejemplo-pipes.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ejemplo-pipes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ejemplo-pipes.component.html',
   styleUrl: './ejemplo-pipes.component.css'
 })
 export class EjemploPipesComponent {
 
+  fechaActual = new Date()
+
+  nombres="job pulache"
+
+  monto=2345234.67788
+
+  porcentaje=0.3567
+
+  persona = {
+    nombre: "Job",
+    apellido: "Pulache",
+    edad: 18,
+    sexo: "Masculino"
+  }
 }
--- src/app/ejemplos-directivas/ejemplos-directivas.component.ts
+++ src/app/ejemplos-directivas/ejemplos-directivas.component.ts
@@ -8,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrl: './ejemplos-directivas.component.css'
 })
 export class EjemplosDirectivasComponent {
+  mostrarAlumnos = false;
+  mostrarCursos = false;
 
+  alumnosLista:  Alumno =[];
+  cursosLista: Curso =[];
+
+
+
+  VerAlumnos(): void {
+    this.mostrarAlumnos = true;
+    this.alumnosLista.push({id: 1, nombre: "Job", apellido: "Rios"});
+    this.alumnosLista.push({id: 2, nombre: "Nick", apellido: "Rios"});
+    this.alumnosLista.push({id: 3, nombre: "laid", apellido: "scock"});  
+    this.alumnosLista.push({id: 4, nombre: "David", apellido: "Rios"});
+  }
+
+  VerCursos(): void {
+    this.mostrarCursos = true;
+    this.cursosLista.push({id:1, nombre:'Des web', precio:200});
+    this.cursosLista.push({id:2, nombre:'app mobile', precio:800})
+    this.cursosLista.push({id:3, nombre:'Base datos 1', precio:200})
+    this.cursosLista.push({id:4, nombre:'Analisis datos', precio:100})
+  }
 }
+
+type Alumno = Array<{
+  id: number;
+  nombre: string;
+  apellido: string
+}>;
+type Curso = Array<{
+  id: number;
+  nombre: string;
+  precio: number;
+}>;
